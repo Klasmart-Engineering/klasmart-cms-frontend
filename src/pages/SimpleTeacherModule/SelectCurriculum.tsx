2e9314b7760cb64a25d91b1be329c3d3bc3f0264
wengcan <572037922@qq.com>
2022-04-21 11:45:36 +0800

    chore: add api for stm

--- src/pages/SimpleTeacherModule/SelectCurriculum.tsx
+++ src/pages/SimpleTeacherModule/SelectCurriculum.tsx
@@ -1,7 +1,7 @@
 import eslImg from "@assets/stm/esl.png";
 import steamImg from "@assets/stm/steam.png";
 import { Box, Link, makeStyles, Typography } from "@material-ui/core";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { pageLinks, StmContext } from "./index";
 
@@ -66,7 +66,6 @@ function CurriculumItem(props: { name: IContextState["curriculum"] }) {
 
 export default function SelectCurriculum() {
   const css = useStyles();
-
   return (
     <Box className={css.root}>
       <Typography className={css.title} variant="h3">
