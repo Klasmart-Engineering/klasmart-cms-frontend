591853aea6543803716ddbb0f9e24f774186ffad
gmj <gengminjie@126.com>
2022-04-27 17:22:16 +0800

    chore: add css style

--- src/pages/SimpleTeacherModule/LessonUnit.tsx
+++ src/pages/SimpleTeacherModule/LessonUnit.tsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles({
     fontWeight: "bold",
     fontVariantNumeric: "lining-nums",
     fontFeatureSettings: "tnum",
+    cursor: "pointer",
   },
   lessonunit: {
     width: vw(320),
