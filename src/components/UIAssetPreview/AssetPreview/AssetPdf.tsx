70d3dec60763d34fb2cf7f0038e1fa11684d0858
Kashish Kumar <kashishkumar@chrysalis.world>
2022-06-09 13:14:34 +0530

    fix(CMS-457): rendering pdf using kl pdf service updated zoom in / zoom out

--- src/components/UIAssetPreview/AssetPreview/AssetPdf.tsx
+++ src/components/UIAssetPreview/AssetPreview/AssetPdf.tsx
@@ -1,17 +1,14 @@
 import { Box, makeStyles, Tooltip, Typography } from "@material-ui/core";
-import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useIntl } from "react-intl";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import fitToHeightIcon from "../../../assets/icons/fit-to-height.svg";
 import fitToWidthIcon from "../../../assets/icons/fit-to-width.svg";
 import zoomInFilledIcon from "../../../assets/icons/zoom-in-filled.svg";
 import zoomInIcon from "../../../assets/icons/zoom-in.svg";
 import zoomOutFilledIcon from "../../../assets/icons/zoom-out-filled.svg";
 import zoomOutIcon from "../../../assets/icons/zoom-out.svg";
-import ReportPagination from "../../ReportPagination/ReportPagination";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-const ROWSPERPAGE = 100;
 const scrollbarWidth = 10;
 const scrollbarHeight = 10;
 const useStyles = makeStyles((theme) => ({
@@ -73,123 +70,148 @@ const useStyles = makeStyles((theme) => ({
 }));
 interface file {
   src: string | undefined;
+  height?: any;
+}
+interface ImageProps {
+  src: string | undefined;
+  width?: string | number;
+  zoomProps?: string | number;
+  height?: string | number;
+}
+async function getPdfMetadata(pdfPath: string | undefined) {
+  var data: number = 0;
+  try {
+    let pdfEndpoint: string = process.env.REACT_APP_KO_BASE_API + `/pdf/` || ``;
+    let response = await axios.get(`${pdfEndpoint}v2/${pdfPath}/metadata`);
+    data = response.data.totalPages;
+  } catch (err) {
+    data = 0;
+  }
+  return data;
+}
+
+export function PdfImage(props: ImageProps) {
+  return (
+    <Box display={"block"} textAlign={"center"}>
+      <LazyLoadImage {...props.zoomProps} effect="blur" className="pdfImage" style={{ maxWidth: 3000 }} src={props.src} />
+    </Box>
+  );
 }
 
 export default function AssetPdf(props: file) {
+  const intl = useIntl();
   const css = useStyles();
-  const [page, setPage] = useState(1);
-  const [numPages, setNumPages] = useState<number>(1);
-  const [scale, setScale] = useState(1);
-  function onDocumentLoadSuccess(pdf: any) {
-    setNumPages(pdf.numPages);
-  }
-  const num = numPages < 100 ? numPages : (page - 1) * ROWSPERPAGE + ROWSPERPAGE > numPages ? numPages - (page - 1) * ROWSPERPAGE : 100;
-
-  const [currentZoomType, setCurrentZoomType] = useState("height-fit");
-
-  const containerRef = useRef<null | HTMLDivElement>(null);
-
-  const maxScale = 4;
-  const minScale = 0.5;
-
-  const zoomIn = () => {
-    setCurrentZoomType("zoom-in");
-    if (scale <= maxScale) {
-      setScale(scale + 0.1);
-    }
-  };
-  const zoomOut = () => {
-    setCurrentZoomType("zoom-out");
-    if (scale >= minScale) {
-      setScale(scale - 0.1);
-    }
-  };
-
+  const [pdfImages, setPdfImages] = useState<string[]>([]);
+  let pdfSrc = props.src;
+  pdfSrc = pdfSrc?.substring(pdfSrc?.lastIndexOf("/") + 1);
+  pdfSrc = pdfSrc?.replace("-", "/");
+  const minScale = 300;
+  const maxScale = 2000;
+  const [currentZoomType, setCurrentZoomType] = useState("width-fit");
   const [zoomProps, setZoomProps]: any = useState({});
+  const [scale, setScale] = useState(0);
 
   useEffect(() => {
-    if (currentZoomType === "height-fit") {
-      let Newheight: any = containerRef?.current?.offsetHeight;
+    if (currentZoomType === "width-fit") {
+      setZoomProps({ width: "100%" });
+    } else if (currentZoomType === "height-fit") {
+      let Newheight: any = document.getElementById("viewContainer")?.offsetHeight;
       setZoomProps(() => ({ height: Newheight }));
-    } else if (currentZoomType === "width-fit") {
-      let NewWidth: any = containerRef?.current?.offsetWidth;
-      NewWidth = NewWidth - scrollbarWidth;
-      setZoomProps(() => ({ width: NewWidth }));
+    } else {
+      setZoomProps({ width: scale });
     }
-  }, [currentZoomType]);
+  }, [currentZoomType, scale]);
 
-  const handleWidthFit = () => {
-    setCurrentZoomType("width-fit");
-    setScale(1);
+  useEffect(() => {
+    if (currentZoomType === "height-fit" || currentZoomType === "width-fit") {
+      let newWidth: any = document.querySelector<HTMLElement>(".pdfImage:first-child")?.offsetWidth;
+      setScale(newWidth);
+    }
+  }, [currentZoomType, zoomProps]);
+
+  const zoomIn = () => {
+    if (scale === undefined) {
+      let newWidth: any = document.querySelector<HTMLElement>(".pdfImage:first-child")?.offsetWidth;
+      setScale(newWidth + 60);
+    } else {
+      if (scale <= maxScale) {
+        setScale(scale + 60);
+      }
+    }
+
+    setCurrentZoomType("zoom-in");
   };
-
+  const zoomOut = () => {
+    if (scale === undefined) {
+      let newWidth: any = document.querySelector<HTMLElement>(".pdfImage:first-child")?.offsetWidth;
+      setScale(newWidth);
+    } else {
+      if (scale >= minScale) {
+        setScale(scale - 60);
+      }
+    }
+    setCurrentZoomType("zoom-out");
+  };
   const handleHeightFit = () => {
     setCurrentZoomType("height-fit");
-    setScale(1);
+  };
+  const handleWidthFit = () => {
+    setCurrentZoomType("width-fit");
   };
 
-  const intl = useIntl();
+  useEffect(() => {
+    getPdfMetadata(pdfSrc).then((data) => {
+      let pageId = 1;
+      let images = [];
+      let pdfEndpoint: string = process.env.REACT_APP_KO_BASE_API + `/pdf/` || ``;
+      while (pageId <= data) {
+        images.push(`${pdfEndpoint}v2/${pdfSrc}/page/${pageId}`);
+        pageId++;
+      }
+      setPdfImages(images);
+    });
+    const container: any = document.getElementById("viewContainer");
+    setScale(container?.offsetWidth);
+  }, [pdfSrc]);
 
   return (
     <>
-      <div className={css.assetsContent} style={{ height: containerRef?.current?.offsetHeight }} ref={containerRef}>
+      <Box
+        style={{
+          overflow: "auto",
+          width: "100%",
+          height: "100%",
+          display: "block",
+        }}
+      >
         <Box className={css.zoomBar}>
-          <Box mt={2} display="inline-block">
-            <Typography className={css.zoomIcons} component="div">
-              <Tooltip
-                title={intl.formatMessage({
-                  id: `library_tooltip_zoom_in`,
-                })}
-              >
-                {currentZoomType === "zoom-in" ? (
-                  <img alt="zoom-in-filled" onClick={zoomIn} className={css.zoomButton} src={zoomInFilledIcon} />
-                ) : (
-                  <img onClick={zoomIn} alt="zoom-in" className={css.zoomButton} src={zoomInIcon} />
-                )}
-              </Tooltip>
-              <Tooltip
-                title={intl.formatMessage({
-                  id: `library_tooltip_zoom_out`,
-                })}
-              >
-                {currentZoomType === "zoom-out" ? (
-                  <img alt="zoom-out-filled" onClick={zoomOut} className={css.zoomButton} src={zoomOutFilledIcon} />
-                ) : (
-                  <img onClick={zoomOut} alt="zoom-out" className={css.zoomButton} src={zoomOutIcon} />
-                )}
-              </Tooltip>
-              <Tooltip
-                title={intl.formatMessage({
-                  id: `library_tooltip_fit_to_width`,
-                })}
-              >
-                <img alt="fit-to-width" onClick={handleWidthFit} className={css.zoomButton} src={fitToWidthIcon} />
-              </Tooltip>
-              <Tooltip
-                title={intl.formatMessage({
-                  id: `library_tooltip_fit_to_height`,
-                })}
-              >
-                <img alt="fit-to-height" onClick={handleHeightFit} className={css.zoomButton} src={fitToHeightIcon} />
-              </Tooltip>
-            </Typography>
-          </Box>
+          <Typography className={css.zoomIcons} component="div">
+            <Tooltip title={intl.formatMessage({ id: `library_tooltip_zoom_in` })}>
+              {currentZoomType === "zoom-in" ? (
+                <img alt="zoom-in-filled" onClick={zoomIn} className={css.zoomButton} src={zoomInFilledIcon} />
+              ) : (
+                <img onClick={zoomIn} alt="zoom-in" className={css.zoomButton} src={zoomInIcon} />
+              )}
+            </Tooltip>
+            <Tooltip title={intl.formatMessage({ id: `library_tooltip_zoom_out` })}>
+              {currentZoomType === "zoom-out" ? (
+                <img alt="zoom-out-filled" onClick={zoomOut} className={css.zoomButton} src={zoomOutFilledIcon} />
+              ) : (
+                <img onClick={zoomOut} alt="zoom-out" className={css.zoomButton} src={zoomOutIcon} />
+              )}
+            </Tooltip>
+            <Tooltip title={intl.formatMessage({ id: `library_tooltip_fit_to_width` })}>
+              <img alt="fit-to-width" src={fitToWidthIcon} onClick={handleWidthFit} className={css.zoomButton} />
+            </Tooltip>
+            <Tooltip title={intl.formatMessage({ id: `library_tooltip_fit_to_height` })}>
+              <img alt="fit-to-height" src={fitToHeightIcon} onClick={handleHeightFit} className={css.zoomButton} />
+            </Tooltip>
+          </Typography>
         </Box>
-        <Document file={props.src} onLoadSuccess={onDocumentLoadSuccess}>
-          {Array.from(new Array(num), (el, index) => (
-            <Page
-              {...zoomProps}
-              scale={scale}
-              className={css.pageCon}
-              key={`page_${index + 1 + (page - 1) * ROWSPERPAGE}`}
-              pageNumber={index + 1 + (page - 1) * ROWSPERPAGE}
-            />
-          ))}
-        </Document>
-      </div>
-      {numPages > ROWSPERPAGE && (
-        <ReportPagination page={page} count={numPages} rowsPerPage={ROWSPERPAGE} onChangePage={(page) => setPage(page)} />
-      )}
+        {pdfImages.map((item, key) => (
+          <PdfImage zoomProps={zoomProps} src={item} key={key} />
+        ))}
+      </Box>
     </>
   );
 }
